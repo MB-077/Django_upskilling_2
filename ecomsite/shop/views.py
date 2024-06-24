5d78c43e86ab1693c2e5d1f4d57ca62b5873a447
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-06-24 23:52:04 +0530

    added new project ecomsite

---
+++ ecomsite/shop/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import *
+from django.core.paginator import Paginator
+
+def index(request):
+    product_objects = Product.objects.all()
+    
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains=item_name)
+    
+    
+    paginator = Paginator(product_objects, 4)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+    
+    return render(request, 'shop/index.html', {'product_objects': product_objects})
+
+
