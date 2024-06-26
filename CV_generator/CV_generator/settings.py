bf3ffddd539f43394c37f059f6e65f53ecf42faa
Marul Bhardwaj <158758744+MB-077@users.noreply.github.com>
2024-06-26 22:58:54 +0530

    updated settings.py

--- CV_generator/CV_generator/settings.py
+++ CV_generator/CV_generator/settings.py
@@ -13,11 +13,12 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+
+    
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
